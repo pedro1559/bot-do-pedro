d0a228d585e4f431ad007fab21d08d246f115f51
pedro1559 <78930227+pedro1559@users.noreply.github.com>
2021-02-13 10:10:24 -0300

    Update help.js

--- src/help.js
+++ src/help.js
@@ -1,10 +1,7 @@
 const help = (prefix) => {
-	return `         *NABUTO LS O BRABO*
-
- DEIXE PELO MENOS MEU CANAL 😔:
+	return `         *Pedro 🤙🐊*
 
 
-CANAL DO DONO: https://youtube.com/channel/UCZEtf9AlsC2zsJQwrfW-44w
 
 
    [𝑨𝑳𝑮𝑼𝑵𝑺 𝑪𝑶𝑴𝑨𝑵𝑫𝑶𝑺 𝑵𝑨̃𝑶 𝑬𝑺𝑻𝑨̃𝑶 𝑭𝑼𝑵𝑪𝑰𝑶𝑵𝑨𝑵𝑫𝑶, 𝑺𝑬 𝑨𝑪𝑯𝑨𝑹 𝑹𝑼𝑰𝑴 𝑬́ 𝑺𝑰𝑴𝑷𝑳𝑬𝑺 𝑷𝑨𝑹𝑨 𝑫𝑬 𝑼𝑺𝑨𝑹, 𝑮𝑹𝑨𝑻𝑶!]
